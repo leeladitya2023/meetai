804a97836c0966a68133131e4b39df26bc96920d
leeladitya2023 <139227589+leeladitya2023@users.noreply.github.com>
2025-06-21 21:05:06 +0530

    Create simple local WhatsApp chat analyzer

---
+++ Desktop/100devs/Basics/intro.js
@@ -0,0 +1,20 @@
+let a=1;
+a=2;
+console.log(a);
+var b=1;
+b=2;
+console.log(b);
+/*const f=8;
+f=90;
+console.log(f);
+*/
+let users=[
+    {
+        first:"kohli",
+        gender:"male",
+    },
+    {
+         first:"ansukha",
+         gender:"female",
+    }
+]
